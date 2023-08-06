6caa8839e6b3146d0369815def777f82b4076de1
Ethan Jones <ethanxjones@gmail.com>
2023-08-06 17:47:58 -0400

    feat: Add "--installCommitHook" as an argument to install commit git hook to project local git folder to enforce Conventional Commits

--- bin/cli.js
+++ bin/cli.js
@@ -4,14 +4,19 @@
  *  Bin file that acts as the main entry point and parses potential CLI arguments
  * 
 */
+const execSync = require('child_process').execSync;
 const conventionalLogs = require('../dist/index');
 let options = [];
-if (process.argv.length > 2) {
-    options = options.concat(process.argv.slice(2));
-} 
-
 try {
-    conventionalLogs(options);
+    if (process.argv.find(argument => argument === '--installCommitHook')) {
+        execSync('cp scripts/commit-msg.sh .git/hooks/ && mv .git/hooks/commit-msg.sh .git/hooks/commit-msg && chmod +x .git/hooks/commit-msg && echo "Commit message githook should now be copied into local git folder"')
+    } else if (process.argv.length > 2) {
+        console.log('User wants to use conventional commits');
+        options = options.concat(process.argv.slice(2));
+        conventionalLogs(options);
+    } else {
+        conventionalLogs(options);
+    }
 } catch (err) {
     console.error(err);
     process.exit(1);
